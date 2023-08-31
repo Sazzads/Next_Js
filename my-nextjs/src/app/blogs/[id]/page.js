4d32c18af65c0928f67e8e09eba0bfa7048326ca
Sazzads <sazzadtk@gmail.com>
2023-08-31 21:59:43 +0600

    add dinamic route

--- my-nextjs/src/app/blogs/[id]/page.js
+++ my-nextjs/src/app/blogs/[id]/page.js
@@ -1,10 +1,27 @@
-import { useParams, useSearchParams } from "next/navigation";
+import loadBlogData from "@/utils/loadBlogData";
+import loadSingleBlogData from "@/utils/loadSingleBlogData";
+export const generateMetadata = async ({ params }) => {
+    const { id, title, body } = await loadSingleBlogData(params.id)
 
-const SingleBlog = ({ params }) => {
+    return {
+        title: `${title} || Next Hero `
+    }
+}
+export const generateStaticParams = async () => {
+    const blogs = await loadBlogData()
+    return blogs.map(({ id }) => ({
+        id:id.toString()
+    }))
+}
+const SingleBlog = async ({ params }) => {
+    const { id, title, body } = await loadSingleBlogData(params.id)
 
     return (
-        <div>
-            single blog {params.id}
+        <div className="container mx-auto">
+            <div className=" border border-blue-500 p-2 my-2 mx-2">
+                <h2 className="text-2xl">{id}. {title} </h2>
+                <p>{body}</p>
+            </div>
         </div>
     );
 };
